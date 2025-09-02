4a4f3f6ebec8d5a3f1806ac64d0780f0e75e3b7c
Chris-Zuniga <cantorzunigac133@mail.sandghills.edu>
2025-09-02 13:31:23 -0400

    Add LAB_1_PROJECT_C.cpp

---
+++ LAB_1_PROJECT_C.cpp
@@ -0,0 +1,25 @@
+//Chris Zuniga
+//Stepember 1, 2025
+//LAB 1 PROJECT (part 3)
+//This is the LAB 1 PROJECT (part 3)
+//Assignment: Design & write a program that takes the square root of a real number and print the result to the console.
+
+
+#include <iostream>
+#include <cmath> // Include cmath for sqrt function
+using namespace std;
+
+int main() {
+
+double numberator= 25.0;
+double result;
+
+
+result = sqrt(numberator);
+
+cout << "The square root of " << numberator << " is " << result << endl;
+
+
+return 0;
+
+}
