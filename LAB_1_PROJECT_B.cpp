d5524fb88355678e7c0cddbcc60ed44c0ff078c2
Chris-Zuniga <cantorzunigac133@mail.sandghills.edu>
2025-09-02 11:11:52 -0400

    Add LAB_1_PROJECT_B.cpp

--- LAB_1_PROJECT_B.cpp
+++ LAB_1_PROJECT_B.cpp
@@ -1,1 +1,26 @@
+//Chris Zuniga
+//August 31, 2025
+//LAB 1 PROJECT (part 2)
+//This is the LAB 1 PROJECT (part 2)
+//Assignment: Design & write a program that divides a number by 0 and review the error.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+double numerator = 10.0;
+double denominator = 0.0;
+double result;
+
+result = numerator / denominator;
+
+cout << "The result of " << numerator << " divided by " << denominator << " is " << result << endl;
+
+return 0;
+
+
+}
+
 
