f6528482a5130c09e9d06ec37580c2de209b47c3
Chris-Zuniga <cantorzunigac133@mail.sandghills.edu>
2025-09-03 10:32:08 -0400

    Add LAB_1_PROJECT_C.cpp

---
+++ LAB_1_PROJECT_D.cpp
@@ -0,0 +1,34 @@
+//Chris Zuniga
+//Stepember 2, 2025
+//LAB 1 PROJECT (part 4)
+//This is the LAB 1 PROJECT (part 4)
+//Assignment: Design & write a program that prints your favorite movie and the main characters neatly to the console.
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string movieTitle;
+
+    movieTitle = "(Avengers: Endgame)";
+
+    cout << "The main charscters in " << movieTitle << " are:" << endl;
+    cout << "Main Character #1: Iorn Man" << endl;
+    cout << "Main Character #2: Captain America" << endl;
+    cout << "Main Character #3: Thor" << endl;
+    cout << "Main Character #4: Hulk" << endl;
+    cout << "Main Character #5: Captain Marvel" << endl;
+    cout << "Main Character #5: Hawkeye" << endl;
+
+    return 0;
+
+
+
+
+
+
+}
+
