a198076289c47b614da61bb0685a6c76f1a374b7
Chris-Zuniga <cantorzunigac133@mail.sandghills.edu>
2025-09-24 11:10:36 -0400

    EXTRA

---
+++ LAB 6 PROJECTS/LAB_6_PROJECT_1.cpp
@@ -0,0 +1,29 @@
+//Chris Zuniga
+//Stepember 23, 2025
+//LAB 5 PROJECT (part 1 of 8)
+//This is the LAB 5 PROJECT (part 1)
+//Assignment: Design and write a program the implements a simple for-loop starting at 0 and terminating at 10. 
+// > Print the loop counter(index) using cout each iteration through the loop.
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //for loop starting at 0 and terminating at 10
+    for (int i = 0; i <= 10; i++)
+    {
+        std::cout <<"Looping until reaches #10: #"<< i << std::endl;
+    }
+
+    // For the message that it has stop looping
+    std::cout << "Looped stopped" << std::endl;
+
+
+    
+
+    return 0;
+
+
+}
---
+++ LAB 6 PROJECTS/LAB_6_PROJECT_2.cpp
@@ -0,0 +1,35 @@
+//Chris Zuniga
+//Stepember 24, 2025
+//LAB 5 PROJECT (part 2 of 8)
+//This is the LAB 5 PROJECT (part 2)
+// Assignment: Design and write a program that accepts user input using cin for an integer value. 
+//  After the user enters their value, use a for-loop to iterate over the integer and print out the number of iterations that occur. 
+//  So for example, if you enter 5, the program should print 0 through 4 on the console.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int userInput;
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+
+}
+
+
+
