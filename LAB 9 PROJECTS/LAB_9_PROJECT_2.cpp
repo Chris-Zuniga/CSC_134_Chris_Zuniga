b2de4e654dc4acfdeda4305e557ddc199f297697
Chris-Zuniga <cantorzunigac133@mail.sandghills.edu>
2025-10-22 11:12:18 -0400

    Updated Project 2 Lab 9

--- LAB 9 PROJECTS/LAB_9_PROJECT_2.cpp
+++ LAB 9 PROJECTS/LAB_9_PROJECT_2.cpp
@@ -4,32 +4,49 @@
 
 #include <iostream>
 #include <fstream> // For file operations
+#include <iomanip> // For setting output precision
 using namespace std;
 
 int main()
 {
-    ofstream outputFile; // Creating an output file stream object
+     // Creating an output file stream object
+    ofstream outputFile;
     int numDays;
     double dailySales;
-    double totalSales = 0.0; // Variable to hold the sum of all daily
 
-    outputFile.open("week9_lab2.txt"); // Opening the output file
+    // Variable to hold the sum of all daily Sales
+    double totalSales = 0.0; 
 
+     // Opening the output file
+    outputFile.open("week9_lab2.txt"); 
+
+        //Prompt user for number of days
     cout << "Enter the number of days: ";
     cin  >> numDays;
 
+    outputFile << fixed << setprecision(2); // Setting output format for currency
 
+    // Loop to get daily sales, write to file, and calculate total sales
     for(int day = 1; day <= numDays; ++day)
     {
+    
         cout << "Enter sales for day " << day << ": $";
         cin >> dailySales;
-        outputFile << "Sales for day " << day << ": $" << dailySales << endl; // Writing daily sales to the file
-        totalSales += dailySales; // Accumulating the total sales
+        // Writing daily sales to the file
+        outputFile << "Sales for day " << day << ": $" << dailySales << endl;
+        // Accumulating the total sales 
+        totalSales += dailySales; 
     }
 
-    outputFile << "Total Sales: $" << totalSales << endl; // Writing the total sales to the file
-    outputFile.close(); // Closing the file
+    // Writing the total sales to the file
+    outputFile << "===========================" << endl;
+    outputFile <<  "Total Sales: $" << totalSales << endl;
+     outputFile << "===========================" << endl;
 
+    // Closing the output file
+    outputFile.close(); 
+
+    // Inform user of successful write operation
     cout << "Sales data and total written to week9_lab2.txt successfully." << endl;
 
     return 0;
